7bef83e6bc03ac5fed24c18cf8ae00048d35e772
LangVE <langve0@gmail.com>
2025-12-01 16:40:45 +0900

    init

---
+++ src/test/java/langve/likelion/devops/sample_ci_server/SampleCiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package langve.likelion.devops.sample_ci_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
